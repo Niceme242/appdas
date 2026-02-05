11f2dbf164170b492ee4578b6f46210b8a62dba3
Niceme242 <marlandking242@gmail.com>
2026-02-05 10:23:59 +0100

    🚀 DÉPLOIEMENT: Version 1.0 apk

--- src/pages/AiForest.js
+++ src/pages/AiForest.js
@@ -78,7 +78,7 @@ const AiForest = () => {
           <svg viewBox="0 0 24 24" width="24" height="24">
             <path fill="#27AE60" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/>
           </svg>
-          <span>Téléchargement réussi. L'alerte Google est normale pour cette version. Vous pouvez continuer en toute confiance.</span>
+          <span>Téléchargement en cours. L'alerte Google est normale pour cette version. Vous pouvez insatller en toute confiance.</span>
         </div>
       `;
       document.body.appendChild(notification);
